e408a52a5c4b09ab611017fe88e887926211265b
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 21:01:30 +0700

    select query

--- index.js
+++ index.js
@@ -11,26 +11,11 @@ const con = mysql.createConnection({
 con.connect((err) => {
   if (err) throw err;
   console.log('Connected to mysql server');
-  let sql = "INSERT INTO customers (name, address) VALUES ?";
-  let values = [
-    ['John', 'Highway 71'],
-    ['Peter', 'Lowstreet 4'],
-    ['Amy', 'Apple st 652'],
-    ['Hannah', 'Mountain 21'],
-    ['Michael', 'Valley 345'],
-    ['Sandy', 'Ocean blvd 2'],
-    ['Betty', 'Green Grass 1'],
-    ['Richard', 'Sky st 331'],
-    ['Susan', 'One way 98'],
-    ['Vicky', 'Yellow Garden 2'],
-    ['Ben', 'Park Lane 38'],
-    ['William', 'Central st 954'],
-    ['Chuck', 'Main Road 989'],
-    ['Viola', 'Sideway 1633']
-  ];
-
-  con.query(sql, [values], (err, res) => {
+  // let sql = "SELECT * FROM customers"; // select all columns
+  let sql = "SELECT name, address FROM customers"; // select some columns
+  con.query(sql, (err, res, fields) => {
     if (err) throw err;
-    console.log('Number of records inserted: ' + res.affectedRows);
+    console.log(res);
+    console.log(res[0]);
   });
 });
