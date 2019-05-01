79b49a8d3a5c18f7f62f279512909241bc3d2832
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 20:49:08 +0700

    creating database

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ const con = mysql.createConnection({
 con.connect((err) => {
   if (err) throw err;
   console.log('Connected to mysql server');
+  con.query("CREATE DATABASE nodejs_mysql_crud", (err, res) => {
+    if (err) throw err;
+    console.log('database created');
+  });
 });
