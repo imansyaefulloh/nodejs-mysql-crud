a4fdd04a038c7ffb7091b68e06e381381f396318
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 21:11:10 +0700

    order by

--- index.js
+++ index.js
@@ -11,20 +11,11 @@ const con = mysql.createConnection({
 con.connect((err) => {
   if (err) throw err;
   console.log('Connected to mysql server');
+  // let sql = "SELECT * FROM customers ORDER BY name";
+  let sql = "SELECT * FROM customers ORDER BY name DESC";
   
-  let address = 'Mountain 21';
-  let sql = "SELECT * FROM customers WHERE address = ?";
-  
-  con.query(sql, [address], (err, res, fields) => {
+  con.query(sql, (err, res, fields) => {
     if (err) throw err;
     console.log(res);
   });
-
-  let name = 'Amy';
-  let address2 = 'Mountain 21';
-  let sql2 = "SELECT * FROM customers WHERE name = ? OR address = ?";
-  con.query(sql2, [name, address2], (err, res, fields) => {
-    if (err) throw err;
-    console.log(res);
-  });  
 });
