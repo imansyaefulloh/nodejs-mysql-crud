116fd910575177c3ee2f42c61270cd3385799671
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 20:56:14 +0700

    insert multiple records

--- index.js
+++ index.js
@@ -11,9 +11,26 @@ const con = mysql.createConnection({
 con.connect((err) => {
   if (err) throw err;
   console.log('Connected to mysql server');
-  let sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-  con.query(sql, (err, res) => {
+  let sql = "INSERT INTO customers (name, address) VALUES ?";
+  let values = [
+    ['John', 'Highway 71'],
+    ['Peter', 'Lowstreet 4'],
+    ['Amy', 'Apple st 652'],
+    ['Hannah', 'Mountain 21'],
+    ['Michael', 'Valley 345'],
+    ['Sandy', 'Ocean blvd 2'],
+    ['Betty', 'Green Grass 1'],
+    ['Richard', 'Sky st 331'],
+    ['Susan', 'One way 98'],
+    ['Vicky', 'Yellow Garden 2'],
+    ['Ben', 'Park Lane 38'],
+    ['William', 'Central st 954'],
+    ['Chuck', 'Main Road 989'],
+    ['Viola', 'Sideway 1633']
+  ];
+
+  con.query(sql, [values], (err, res) => {
     if (err) throw err;
-    console.log('1 record inserted');
+    console.log('Number of records inserted: ' + res.affectedRows);
   });
 });
